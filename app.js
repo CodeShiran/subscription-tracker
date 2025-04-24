d1fdc3c3ce608501bad3df8138cba93315f2dd7e
Shiran_lakshitha <shiranlakshithareal@gmail.com>
2025-04-24 10:46:59 +0530

    runs locally

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hello  world')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('hi shiran')
+})
+
+app.listen(3000, () => {
+    console.log('server running on 3000')
+})
